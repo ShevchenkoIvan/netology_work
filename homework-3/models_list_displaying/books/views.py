a8df316df30aa49cafb3bb1ceabb0c5631532b27
IvSh <example@exmp.com>
2022-05-03 20:54:53 +0300

    Дополнительное задание

--- homework-3/models_list_displaying/books/views.py
+++ homework-3/models_list_displaying/books/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Book
+
+
+def index(request):
+    return redirect('books')
 
 
 def books_view(request):
+    list_books = Book.objects.all()
     template = 'books/books_list.html'
-    context = {}
+    context = {
+        'books': list_books
+    }
+    return render(request, template, context)
+
+
+def show_book(request, pub_date):
+    template = 'books/books_list.html'
+    selected_book = Book.objects.filter(pub_date=pub_date)
+
+    previous_date = Book.objects.filter(pub_date__lt=pub_date).order_by('-pub_date').first()
+    next_date = Book.objects.filter(pub_date__gt=pub_date).order_by('pub_date').first()
+
+    context = {
+        'books': selected_book,
+        'previous_date': previous_date,
+        'next_date': next_date,
+    }
     return render(request, template, context)
