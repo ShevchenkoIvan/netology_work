ea8275876b437f9c2b253cb5b705e73f2f77454b
IvSh <example@exmp.com>
2022-05-11 16:29:57 +0300

    HomeWork - 6

---
+++ homework-6/logistic/serializers.py
@@ -0,0 +1,62 @@
+from rest_framework import serializers
+from .models import Product, StockProduct, Stock
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions']
+
+    # настройте сериализатор для склада
+
+    def create(self, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # создаем склад по его параметрам
+        stock = super().create(validated_data)
+
+        # здесь вам надо заполнить связанные таблицы
+        for item in positions:
+            StockProduct.objects.create(
+                stock=stock,
+                product=item.get('product'),
+                quantity=item.get('quantity'),
+                price=item.get('price'),
+            )
+
+        return stock
+
+    def update(self, instance, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # обновляем склад по его параметрам
+        stock = super().update(instance, validated_data)
+
+        # здесь вам надо обновить связанные таблицы
+        for item in positions:
+            StockProduct.objects.update_or_create(
+                stock=stock,
+                product=item.get('product'),
+                defaults={
+                    'quantity': item.get('quantity'),
+                    'price': item.get('price')
+                }
+            )
+
+        return stock
